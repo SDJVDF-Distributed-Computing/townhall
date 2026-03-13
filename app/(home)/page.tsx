087322d153cad5d75708a42d58209c47041b20a3
Jasongtsk <150237672+Jasongtsk@users.noreply.github.com>
2026-03-13 14:22:52 +0000

    feat: add homepage with message feed

---
+++ app/(home)/page.tsx
@@ -0,0 +1,25 @@
+"use client"
+import { AppSidebar } from "@/components/app-sidebar"
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+import { MessageFeed } from "@/src/message/components/message-feed"
+import { NewChatForm } from "@/src/message/form/new-chat.form"
+import { Header } from "@/src/shared/components/header/header"
+
+export default function Page() {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset className="block">
+        <div className="flex h-svh flex-col">
+          <Header />
+          <main className="flex-1 overflow-y-auto">
+            <MessageFeed />
+          </main>
+          <footer className="shrink-0 border-t p-4">
+            <NewChatForm />
+          </footer>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
